d5ac0ea2fdbf504d56400d67bed5d1ba7d59898c
Humairaa1 <humaira71bd@gmail.com>
2023-08-17 13:59:56 +0600

    Finish

--- bank.js
+++ bank.js
@@ -1,4 +1,6 @@
-//get the deposit Button here
+//---------- Function of Deposit Button-------
+
+//get the deposit Button here and set event handler
 document.getElementById('btn-deposit').addEventListener('click',function(){
     //get the deposit input field and it's value
     const depositField = document.getElementById('deposit-field');
@@ -23,7 +25,40 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
     
     //set total balance
     balanceTotalElement.innerText = totalBalanceNow;
-    
+
     //clear deposit field
     depositField.value = '';
+})
+
+//---------Function of Withdraw button----------
+
+//get the withdraw button and set event handler
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+
+    //get withdraw input field and it's value
+    const withdrawField = document.getElementById('withdraw-field');
+    const withdrawAmount = withdrawField.value;
+
+    //get withdraw total and its innerText
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const withdrawTotalPrevious = withdrawTotalElement.innerText;
+
+    // Add Total withdraw amount 
+    const withdrawTotal = parseFloat(withdrawTotalPrevious) + parseFloat(withdrawAmount);
+
+    //set total withdraw amount
+    withdrawTotalElement.innerText=withdrawTotal;
+
+     //get previous balance and it's innerText 
+     const balanceTotalElement =document.getElementById('Balance-total');
+     const previousBalance = balanceTotalElement.innerText;
+ 
+     //add current total balance here
+     const totalBalanceNow = parseFloat(previousBalance) - parseFloat(withdrawAmount);
+     
+     //set total balance
+     balanceTotalElement.innerText = totalBalanceNow;
+
+    //clear withdraw field
+    withdrawField.value = '';
 })
